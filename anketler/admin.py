804a175922b05de9b009bbf4de52ada9fb4cf00f
ayberk çağatay arat <ayberkarat78@gmail.com>
2026-03-24 21:14:39 +0300

    Bolum 7: Admin paneli ozellestirildi ve superuser olusturuldu

--- anketler/admin.py
+++ anketler/admin.py
@@ -1,5 +1,28 @@
 from django.contrib import admin
 from .models import Soru, Secenek
 
-admin.site.register(Soru)
-admin.site.register(Secenek)
+# 1. Seçenekleri sorunun içine gömmek için bir "satır içi" sınıfı oluşturuyoruz
+class SecenekEkleme(admin.TabularInline):
+    model = Secenek
+    extra = 3  # Soru eklerken altta otomatik 3 tane boş seçenek kutusu açar
+
+# 2. Soru panelinin nasıl görüneceğini ayarlıyoruz
+class SoruAdmin(admin.ModelAdmin):
+    # Formdaki alanların sırasını ve gruplarını belirliyoruz
+    fieldsets = [
+        (None, {"fields": ["soru_metni"]}),
+        ("Tarih Bilgisi", {"fields": ["yayin_tarihi"], "classes": ["collapse"]}),
+    ]
+    inlines = [SecenekEkleme] # Seçenekleri buraya bağladık
+    
+    # Listeleme sayfasındaki sütunlar
+    list_display = ["soru_metni", "yayin_tarihi", "yakin_zamanda_yayinlandi_mi"]
+    
+    # Sağ tarafa filtreleme paneli
+    list_filter = ["yayin_tarihi"]
+    
+    # Üst tarafa arama kutusu
+    search_fields = ["soru_metni"]
+
+# 3. Hazırladığımız bu ayarları Django'ya kaydediyoruz
+admin.site.register(Soru, SoruAdmin)
--- config/urls.py
+++ config/urls.py
@@ -5,3 +5,6 @@ urlpatterns = [
     path("anketler/", include("anketler.urls")),
     path("admin/", admin.site.urls),
 ]
+admin.site.site_header = "PANEL İŞTE "   
+admin.site.site_title = "AYBOOOOO"             
+admin.site.index_title = "ANKET İŞTE DOLDUR KAPAT"
